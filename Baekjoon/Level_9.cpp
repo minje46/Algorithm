ece22c58f62b95206242adf74723687f1a40cff7
minje46 <minjea1701@naver.com>
2018-10-08 07:50:32 +0900

    acmicp / step_9
    The most important thing is usage of vector and allocation of array in dynamic.

--- Baekjoon/Level_9.cpp
+++ Baekjoon/Level_9.cpp
@@ -1,9 +1,8 @@
+// 9 - 2 (Merge_Sort) <- Array 로 구현
+/*
 #include <iostream>
 using namespace std;
 
-
-// 9 - 2 (Merge_Sort) <- Array 로 구현
-/*
 #define Size 1000005
 int input[Size];
 int output[Size];
@@ -79,6 +78,9 @@ int main()
 
 // 9 - 2 (Heap_Sort) <- Array 로 구현
 /*
+#include <iostream>
+using namespace std;
+
 #define Size 1000005
 int arr[Size];
 
@@ -148,6 +150,9 @@ int main()
 
 // 9 - 3 (Counting_Sort) <- Array 로 구현
 /*
+#include <iostream>
+using namespace std;
+
 #define Size 10001
 int arr[Size] = { 0, };
 
@@ -181,3 +186,188 @@ int main(void)
 */
 
 // 9 - 4
+/*
+#include <iostream>
+#include<vector>
+#include<algorithm>
+#include<cmath>
+
+using namespace std;
+
+int Range(vector<int> v)
+{
+	int max = v.front(), min = v.front();		// Initialize the max and min value.
+	vector<int>::iterator iter = v.begin();		// The iterator of vector.
+	
+	for (iter; iter != v.end(); iter++)
+	{
+		if (max < *iter)
+			max = *iter;
+
+		if (min > *iter)
+			min = *iter;
+	}
+	
+	return (max - min);
+}
+
+bool compare(const pair<int, int> &p1, const pair<int, int> &p2)
+{
+	if (p1.second == p2.second)
+		return p1.first < p2.first;			// This condition means comparing the second value which means counting of value.
+												// And if there are some values which have same counting value, it returns smaller value first.
+	return p1.second > p2.second;		// It compares the counting value of vectors.
+}
+
+int Mode(vector<int> v)
+{
+	vector<pair<int, int>> count;
+	vector<int>::size_type i;
+
+	sort(v.begin(), v.end());
+
+	for (i = 0; i < v.size(); i++)
+	{
+		if (count.empty())		// It means when i is 0. 
+		{							// When the count vector is empty, set the first element in count vector.
+			count.push_back(pair<int, int>(v[i], 1));
+			continue;				// After continue condition, it will be started at i = 1 which means second index.
+		}
+
+		if (count.back().first == v[i])		// Check the next value is duplicate or not. 
+		{										// The reason why it is possible is original vecotr v is already sorted once.
+			pair<int, int> temp = count.back();	// If the value of count vector is duplicate with next value of v vector which will be set next time. 
+			temp.second++;				// It increase the counting in vector's second element which means for counting of value.
+			count.pop_back();				// To update modified value which was increased the counting from previous count vector.
+			count.push_back(temp);		// Set new modified vector.
+		}
+		else
+			count.push_back(pair<int, int>(v[i], 1));		// It means there is no duplicate value in original v vector. 
+	}
+
+	sort(count.begin(), count.end(), compare);				// Sort with mode. (Depends on count how many times it is used.)
+
+	if (count[0].second == count[1].second)
+		return count[1].first;									// It means the second small value with the largest count value.
+	else
+		return count[0].first;									// It means the value of largest count value.
+}
+
+int Median(vector<int> v)
+{
+	sort(v.begin(), v.end());
+
+	return v[(0 + v.size()) / 2];
+}
+
+double Sum(vector<int> v)
+{
+	double result = 0;
+
+	for (int i = 0; i < v.size(); i++)
+		result += v[i];
+
+	result = round(result / v.size());
+
+	return result;
+}
+
+int main(void)
+{
+	std::ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+
+	vector<int> v;		// Allocate vecotr memory by int type.
+	int value;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> value;
+		v.push_back(value);	// Allocate the value in end of vector.
+	}
+
+	cout << Sum(v) << endl;
+	cout << Median(v) << endl;
+	cout << Mode(v) << endl;
+	cout << Range(v) << endl;
+}
+*/
+
+// 9 - 5
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+
+using namespace std;
+
+int main(void)
+{
+	std::ios_base::sync_with_stdio(false);
+
+	string str;
+	cin >> str;
+	
+	sort(str.begin(), str.end(), greater<char>());
+
+	for (auto c : str)
+		cout << c;
+	cout << endl;
+}
+
+
+// 9 - 6
+/*
+#include<iostream>
+#include<string>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+bool compare(string first, string second)
+{
+	// The first condition of sorting is the length of string.
+	if (first.length() < second.length())
+		return true;
+	else if (first.length() == second.length())		// If the length of string are same, the alphabetic order is the second standard of sorting.
+	{														// The smaller one is sorted frist.
+		if (first < second)	
+			return true;
+		else
+			return false;
+	}
+	else
+		return false;
+}
+
+int main(void)
+{
+	std::ios_base::sync_with_stdio(false);
+
+	int N;
+	cin >> N;
+
+	string temp;
+	vector<string> v;
+
+	for (int i = 0; i < N; i++)
+	{
+		cin >> temp;
+		v.push_back(temp);
+
+		// To remove duplicate value in v vector with searching from begin to end - 1.
+		for (int i = 0; i < v.size()-1; i++)
+			if (temp == v[i])
+				v.pop_back();
+	}
+
+	sort(v.begin(), v.end(), compare);
+
+	for (int i = 0; i < v.size(); i++)
+		cout << v[i] << "\n";
+}
+*/
